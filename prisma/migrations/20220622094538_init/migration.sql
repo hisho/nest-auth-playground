254cdaa729c4c8d9be360c05e02fecf22e1bf0ef
hisho <hisho.web@gmail.com>
2022-06-22 19:16:58 +0900

    🎉 prismaをnestで使う初期設定

---
+++ prisma/migrations/20220622094538_init/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `Post` table. All the data in the column will be lost.
+  - Added the required column `content` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `Post` DROP COLUMN `description`,
+    ADD COLUMN `content` VARCHAR(191) NOT NULL,
+    ADD COLUMN `published` BOOLEAN NOT NULL DEFAULT false;
