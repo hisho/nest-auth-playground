72bb461277176259882764840094c07b6227f060
hisho <hisho.web@gmail.com>
2022-06-23 18:42:51 +0900

    🚚 UserEntity -> User

--- src/module/user/entities/user.entity.ts
+++ src/module/user/entities/user.entity.ts
@@ -2,7 +2,7 @@ import { ObjectType, PickType } from '@nestjs/graphql';
 import { User as UserModel } from '../../../prisma/@generated/prisma-nestjs-graphql/user/user.model';
 
 @ObjectType()
-export class UserEntity extends PickType(UserModel, [
+export class User extends PickType(UserModel, [
   'id',
   'uuid',
   'name',
--- src/module/user/user.resolver.ts
+++ src/module/user/user.resolver.ts
@@ -1,18 +1,18 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { UserService } from './user.service';
-import { UserEntity } from './entities/user.entity';
+import { User } from './entities/user.entity';
 import { CreateUserInput } from './dto/create-user.input';
 
-@Resolver(() => UserEntity)
+@Resolver(() => User)
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
-  @Query(() => [UserEntity])
+  @Query(() => [User])
   users() {
     return this.userService.findAll();
   }
 
-  @Mutation(() => UserEntity)
+  @Mutation(() => User)
   createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
     return this.userService.create(createUserInput);
   }
